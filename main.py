298649fed4f3658bd24dfe940b1b9faa2f5d9613
Shivam Singh <shivamsingh94192@gmaill.com>
2021-03-30 02:09:15 -0400

    Added code to count Day/Night chargers and plot using seaborn.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from reading_data import data_meter_values, data_open_trans
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 df_open_transactions = data_open_trans()
 df_meter_values = data_meter_values()
@@ -10,5 +12,6 @@ print(df_open_transactions['TransactionStartTime'].head())
 print(df_open_transactions['TransactionStopTime'].head())
 
 print((df_open_transactions['StartCard'].nunique()))
-
 print((df_open_transactions.groupby(['Day/Night']).sum()))
+sns.countplot(df_open_transactions['Day/Night'], label='Count of EV Charging Day/Night transaction ')
+plt.show()
