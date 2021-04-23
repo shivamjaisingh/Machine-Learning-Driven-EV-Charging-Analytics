53204dde77ce5f8dc0058b95ee9a96547d0c34e5
Shivam Singh <shivamsingh94192@gmaill.com>
2021-04-23 13:47:06 -0400

    Added code for GMM clustering.

--- gmm_model.py
+++ gmm_model.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from sklearn.mixture import GaussianMixture
 from datetime import datetime
 import pandas as pd
@@ -22,5 +23,31 @@ print(df_open_transactions.columns.values)
 
 data = df_open_transactions[['Start Integer Hour_P', 'ConnectedTime']]
 
+plt.scatter(data['Start Integer Hour_P'], data['ConnectedTime'], s=2)
+plt.title('Start Time and Total Connected Time')
+plt.xlabel('Start Connection Hour ')
+plt.ylabel('Total Hours Connected')
+plt.show()
 
-gmm = GaussianMixture(n_components=2, covariance_type='full').fit(data)
+gmm = GaussianMixture(n_components=9, covariance_type='full').fit(data)
+# print(gmm.means_)
+# print('\n')
+# print(gmm.covariances_)
+
+# predictions from kmeans
+labels = gmm.predict(data)
+print(labels)
+frame = pd.DataFrame(data)
+frame['cluster'] = labels
+frame.columns = ['Start Integer Hour_P', 'ConnectedTime', 'cluster']
+
+# plotting results
+color = ['green', 'red', 'blue', 'cyan', 'magenta', 'yellow', 'black', 'orange', 'pink']
+for k in range(0, 9):
+    data = frame[frame["cluster"] == k]
+    plt.scatter(data['Start Integer Hour_P'], data['ConnectedTime'], c=color[k], s=2)
+
+plt.title('GMM Visualization with 9 clusters of charging sessions')
+plt.xlabel('Start Connection Hour ')
+plt.ylabel('Total Hours Connected')
+plt.show()
