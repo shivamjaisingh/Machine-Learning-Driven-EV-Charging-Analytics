6f629570ed61f701a4161dec6f9656fe1cfeff20
Shivam Singh <shivamsingh94192@gmaill.com>
2021-04-26 02:23:17 -0400

    Added graph.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ df_open_transactions = data_open_trans()
 # df_meter_values = data_meter_values()
 
 print(df_open_transactions.columns.values)
+
 # print(df_meter_values.columns.values)
 
 # print(df_open_transactions.describe(datetime_is_numeric=True, include="all").to_string())
@@ -23,10 +24,31 @@ print(df_open_transactions.columns.values)
 # print((df_open_transactions.groupby(['Day/Night'])['MaxPower'].sum()))
 # # first graph
 # print(df_open_transactions.groupby(['Start Integer Hour'])['TransactionId'].count())
-# df = pd.DataFrame(df_open_transactions.groupby(['Start Integer Hour'])['TransactionId'].count())
+start_time = pd.to_datetime(df_open_transactions['UTCTransactionStart'])
+
+
+def creating_hour_values(transaction_start_time):
+    return int(float(transaction_start_time.strftime("%H")) + float(transaction_start_time.strftime("%M")) / 60)
+
+
+df_open_transactions['Start Integer Hour_P'] = start_time.apply(lambda row: creating_hour_values(row))
+df = pd.DataFrame(df_open_transactions.groupby(['Start Integer Hour_P'])['TransactionId'].count())
+print(df.shape)
+y = pd.DataFrame(df['TransactionId'], columns=['hours'])
+x = range(24)
 # dd = df.plot()
 # dd.set_xticks(range(len(df)))
-# plt.show()
+# plt.figure(figsize=(40,20))
+fig=plt.figure()
+fig.patch.set_facecolor('xkcd:lightblue')
+plt.grid()
+plt.plot(x, df, linestyle='solid', color='black')
+plt.scatter(pd.DataFrame(range(24)), df, s=85, edgecolors='deeppink', c='black')
+plt.xlabel('Time of the day')
+plt.ylabel('Total Transactions')
+plt.title('Charging transactions per hour of the day')
+plt.xlim(-2, 25)
+plt.show()
 # # first graph
 #
 # chargeTime = df_open_transactions['ChargeTime'] > 0
@@ -131,4 +153,3 @@ print(df_open_transactions.columns.values)
 # print(df_open_transactions['Peaks_Python'])
 #
 # df_open_transactions.to_csv("dataset_new", sep=',', encoding='utf-8')
-
